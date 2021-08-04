0a6d9b8dcc04f827c49c24810624c7a344f3a153
Gino Ascencio <gino.francisco@hotmail.com>
2021-08-03 20:08:30 -0500

    Terminado

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ import array
 import random
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://bbbc511db8d449:e6f1c3ff@us-cdbr-east-04.cleardb.com/heroku_dcd450de6ec7181'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/viajerotsp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://bbbc511db8d449:e6f1c3ff@us-cdbr-east-04.cleardb.com/heroku_dcd450de6ec7181'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/viajerotsp'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
